7e45f8df1614261bc897b98865a019371f187bbb
Jakub Gereg <j.gereg@gmail.com>
2020-05-13 10:42:07 +0200

    updated test

--- test.js
+++ test.js
@@ -1,12 +1,6 @@
-const d = require(".");
+const c = require(".");
 
-it("should draw one element from array", () => {
-  let colors = ["red", "blue", "green"];
-  d(colors);
-  expect(colors).toHaveLength(2);
-});
-
-it("should draw different element each time", () => {
-  let colors = ["red", "blue", "green", "yellow", "teal"];
-  expect(d(colors)).not.toEqual(d(colors));
+it("should get all 52 cards", () => {
+  let cards = c.getAll();
+  expect(cards).toHaveLength(52);
 });
